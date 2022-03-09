2125724a3db904125a06b2c7065199157f9a2883
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-09 16:36:40 -0600

    finished project

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -87,8 +87,60 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Will I ever learn this stuff. ',
+    date: 'March, 9th, 2022',
+    firstParagraph: `🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 🤞 💩`,
+
+    secondParagraph: `If not I guess I can always marry rich. `,
+
+    thirdParagraph: `Or more likely become a goshdarn truck driver. `
   }
 ];
+
+
+
+function articleMaker(articleObj) {
+  const articleWrapper = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const articleParagraphOne = document.createElement('p');
+  const articleParagraphTwo = document.createElement('p');
+  const articleParagraphThree = document.createElement('p');
+  const expandButton = document.createElement('span');
+
+
+  articleWrapper.classList.add('article');
+  articleTitle.classList.add('date');
+  expandButton.classList.add('expandButton');
+
+  articleWrapper.appendChild(articleTitle);
+  articleWrapper.appendChild(articleDate);
+  articleWrapper.appendChild(articleParagraphOne);
+  articleWrapper.appendChild(articleParagraphTwo);
+  articleWrapper.appendChild(articleParagraphThree);
+  articleWrapper.appendChild(expandButton);
+
+  articleTitle.textContent = articleObj.title;
+  articleDate.textContent = articleObj.date;
+  articleParagraphOne.textContent = articleObj.firstParagraph;
+  articleParagraphTwo.textContent = articleObj.secondParagraph;
+  articleParagraphThree.textContent = articleObj.thirdParagraph;
+  expandButton.textContent = "+";
+
+
+  expandButton.addEventListener('click', () => {
+    articleWrapper.classList.toggle('article-open')
+  })
+
+return articleWrapper;
+
+} 
+
+data.forEach(article => {
+  document.querySelector('div.articles').appendChild(articleMaker(article));
+})
 
 /*
   Step 1: Write a component called 'articleMaker' to create an article.
